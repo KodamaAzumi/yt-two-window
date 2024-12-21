a023f5f010b430d837a2a07560864c5bbb570d14
Kodama Azumi <kodamano0621@gmail.com>
2024-12-21 18:57:42 +0900

    add file

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,144 @@
-import Image from "next/image";
+"use client";
+import { useRef, useState } from "react";
+import YtTwoWindows from "../components/yt-two-windows";
+import YtOneWindows from "../components/yt-one-windows";
+import YtThreeWindows from "@/components/yt-three-windows";
+import YtFourWindows from "@/components/yt-four-widonws";
+import YtFiveWindows from "@/components/yt-five-widonws";
+import YtSixWindows from "@/components/yt-six-windows";
+
+const extractYouTubeID = (url) => {
+  const regex =
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|live\/)|youtu\.be\/)([\w-]{11})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const inputRef = useRef(null);
+  const [videoIds, setVideoIds] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const handleAddVideoIds = (e) => {
+    e.preventDefault();
+    const currentVideoId = extractYouTubeID(inputRef.current.value);
+    if (currentVideoId && videoIds.length < 6) {
+      const newVideoIds = [...videoIds, currentVideoId];
+      setVideoIds(newVideoIds);
+      console.log(newVideoIds);
+      inputRef.current.value = "";
+    }
+  };
+
+  if (videoIds.length == 1) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtOneWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else if (videoIds.length == 2) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtTwoWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else if (videoIds.length == 3) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtThreeWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else if (videoIds.length == 4) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtFourWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else if (videoIds.length == 5) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtFiveWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else if (videoIds.length == 6) {
+    return (
+      <div className="flex flex-col items-center">
+        <YtSixWindows videoIds={videoIds} />
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="動画のURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-end h-screen p-6">
+        <form onSubmit={handleAddVideoIds}>
+          <input
+            placeholder="YouTubeのURLを入力"
+            className="focus:outline-none bg-orange-200 mr-4 p-2 rounded-md"
+            ref={inputRef}
+          ></input>
+          <button className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-300">
+            追加
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
---
+++ src/components/yt-six-windows.jsx
@@ -0,0 +1,117 @@
+"use client";
+import { useState } from "react";
+import YouTube from "react-youtube";
+
+export default function YtSixWindows({ videoIds }) {
+  const [YTPlayers, setYTPlayers] = useState([]);
+
+  const makeYTPlayers = (e) => {
+    const newYTPlayers = [...YTPlayers, e.target];
+    setYTPlayers(newYTPlayers);
+  };
+
+  const handlePlayVideo = () => {
+    YTPlayers.forEach((YTPlayer) => {
+      if (YTPlayer.g) {
+        YTPlayer.playVideo();
+      }
+    });
+  };
+
+  const handlePauseVideo = () => {
+    YTPlayers.forEach((YTPlayer) => {
+      if (YTPlayer.g) {
+        YTPlayer.pauseVideo();
+      }
+    });
+  };
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="h-[calc(100vh-80px-40px)] flex flex-col items-center">
+        <div className="flex w-full justify-center">
+          <YouTube
+            videoId={videoIds[0]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+          <YouTube
+            videoId={videoIds[1]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+          <YouTube
+            videoId={videoIds[2]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+        </div>
+        <div className="flex w-full justify-center">
+          <YouTube
+            videoId={videoIds[3]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+          <YouTube
+            videoId={videoIds[4]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+          <YouTube
+            videoId={videoIds[5]}
+            onReady={makeYTPlayers}
+            opts={{ width: "100%", height: "auto" }}
+            className="w-1/3"
+            iframeClassName="aspect-video"
+          />
+        </div>
+      </div>
+      <div className="flex items-center py-4 justify-center">
+        <button
+          className="bg-sky-400 rounded-full p-3 mr-4 hover:bg-sky-300"
+          onClick={handlePlayVideo}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <button
+          className="bg-sky-400 rounded-full p-3 mr-4 hover:bg-sky-300"
+          onClick={handlePauseVideo}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
